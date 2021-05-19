a40414107e20f61d29dcc87bad65aea8cad47eda
Anshika <anshikasethiya28@gmail.com>
2021-05-19 18:28:00 +0530

    added the feature of dispaly game history and jump to a move by clicking on history

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import "./styles/root.scss"
 import Board from "./components/Board"
+import { useState } from 'react'
+import { calculateWinner } from "./helper";
+import History from "./components/History"
 
 function App() {
+  // const [board, setBoard] = useState(Array(9).fill(null));
+  // const [isXNext, setIsXNext] = useState(false);
+
+  const [history, setHistory] = useState([
+    {board: Array(9).fill(null), isXNext : true},
+  ]); 
+
+  const [currentMove, setCurrentMove] = useState(0);
+
+  const current = history[currentMove]
+
+  const winner = calculateWinner(current.board);
+  const message = winner ? `Winner is ${winner}` : `Next player is ${current.isXNext ? 'X' : 'O'}`;
+
+  const handleSquareClick = position => {
+      if(current.board[position] || winner){
+          return;
+      }
+      setHistory(prev => {
+        const last = prev[prev.length -1]
+          const newBoard = last.board.map((square, pos) => {
+              if(pos === position){
+                  return last.isXNext ? 'X' : 'O'
+              }
+              return square;
+          });
+          return prev.concat({board:newBoard, isXNext: !last.isXNext})
+      });
+      setCurrentMove(prev => prev + 1);
+  };
+const moveTo = move => {
+  setCurrentMove(move)
+}
+
   return (
   <div className="app">
     <h1>TIC TAC TOE</h1>
-    <Board />
+    <h2>{ message }</h2>
+    <Board board={current.board} handleSquareClick={handleSquareClick} />
+    <History history={history} moveTo = {moveTo} currentMove={currentMove}/>
   </div>
   );
 }
